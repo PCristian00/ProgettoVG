bd0087de8b12ed3e3be00f0f602ebb62cb574bca
PCristian00 <cristianp9300@gmail.com>
2023-05-19 18:58:00 +0200

    Nuovo movimento, migliorie Spawner
    Spawner più preciso, movimento astronave libero

--- Assets/Scripts/ShipControllerScript.cs
+++ Assets/Scripts/ShipControllerScript.cs
@@ -2,51 +2,57 @@ using UnityEngine;
 
 public class ShipControllerScript : MonoBehaviour
 {
-    //Posizione dopo spostamento
-    private Vector3 endPos;
 
-    
-           
+   //Posizione dopo spostamento
+    private Vector3 endPos = new(0, -4, 0);
+
     //Quantita di spostamento.
     //La corsia ha 6 di larghezza, NON cambiare.
     public int deltaX = 6;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float speed=10f;
 
     // Update is called once per frame
     void Update()
     {
         //Spostamento verso destra
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             //Si sposta solo se non si trova già al bordo
             if (transform.position.x < 6)
             {
-                ChangeLane(deltaX);
+                //ChangeLane(deltaX);
+                Move(speed);
             }       
         }
 
         //Spostamento verso sinistra
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //Si sposta solo se non si trova già al bordo
             if (transform.position.x > -6)
             {
-                ChangeLane(-deltaX);
+                //ChangeLane(-deltaX);
+
+                Move(-speed);
             }
         }
     }
 
-    //Cambio corsia (sinistra e destra)
-    void ChangeLane(int deltaX)
-    {
-        endPos = new Vector3(transform.position.x + deltaX, transform.position.y, 0);
-        gameObject.transform.position = endPos;
+    /*
+     * Cambio corsia (sinistra e destra) fissato a centro colonna
+     * 
+     void ChangeLane(int deltaX)
+     {
+         endPos = new Vector3(transform.position.x + deltaX, transform.position.y, 0);
+         gameObject.transform.position = endPos;        
+     }
+    
+     */
 
-       
+    //Movimento libero fluido
+    private void Move(float speed)
+    {
+        endPos.x += speed * Time.deltaTime;
+        gameObject.transform.position = endPos;
     }
 }
