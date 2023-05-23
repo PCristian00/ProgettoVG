4a6467a94922df593e7dd7f1883de5054b16cd2a
PCristian00 <cristianp9300@gmail.com>
2023-05-23 16:40:25 +0200

    ClearLog (per Debug)
    Aggiunta funzione che svuota il Debug Log ad ogni riavvio del gioco.

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -1,23 +1,24 @@
+using System.Reflection;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
-    //Punteggio del giocatore
+    // Punteggio del giocatore
     public int playerScore;
-    //Casella di testo che mostra il punteggio attuale
+    // Casella di testo che mostra il punteggio attuale
     public TextMeshProUGUI scoreText;
-    //Casella di testo che mostra il punteggio migliore di sempre
+    // Casella di testo che mostra il punteggio migliore di sempre
     public TextMeshProUGUI highScoreText;
-    //Schermata da caricare quando le vite finiscono
+    // Schermata da caricare quando le vite finiscono
     public GameObject gameOverScreen;
-    //Suono da attivare per ogni punto ottenuto
+    // Suono da attivare per ogni punto ottenuto
     public AudioSource scoreEffect;
-    //Musica di sottofondo
+    // Musica di sottofondo
     public AudioSource backGroundMusic;
 
-    //Velocita' di gioco (tasso di spawn ostacoli)
+    // Velocita' di gioco (tasso di spawn ostacoli)
     public float speed = 0f;
     public float maxSpeed = 2f;
     public float minSpeed = 0f;
@@ -27,7 +28,7 @@ public class LogicScript : MonoBehaviour
     private void Start()
     {
         playerScore = 0;
-        //Preleva il punteggio migliore dai salvataggi di Unity
+        // Preleva il punteggio migliore dai salvataggi di Unity
         highScore = PlayerPrefs.GetInt("highscore", highScore);
         highScoreText.text = highScore.ToString();
     }
@@ -46,6 +47,9 @@ public class LogicScript : MonoBehaviour
 
     public void RestartGame()
     {
+        // Svuota il DebugLog prima di riavviare la scena
+        // Forse inutile in gioco finale
+        ClearLog();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
@@ -81,17 +85,26 @@ public class LogicScript : MonoBehaviour
 
     public void CheckDifficulty()
     {
-        //Se il punteggio e' multiplo di 10
+        // Se il punteggio e' multiplo di 10
         if (playerScore % 10 == 0)
         {
-            //Aumento velocita'
+            // Aumento velocita'
             ChangeSpeed(1);
-            //Aumento velocita' minima
+            // Aumento velocita' minima
             if (minSpeed < maxSpeed)
             {
                 minSpeed += 0.25f;
                 Debug.Log("NUOVA VELOCITA' MINIMA: " + minSpeed);
             }
         }
+    }
+
+    // Svuota il DebugLog
+    public void ClearLog()
+    {
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
     }
 }
