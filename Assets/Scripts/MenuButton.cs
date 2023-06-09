43952df604d7769a267a1cc3c0520d251e081d1d
PCristian00 <cristianp9300@gmail.com>
2023-06-09 21:15:57 +0200

    Test con 3 bottoni

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
 
     public SpriteRenderer spriteRenderer;
+
+    public TextMeshProUGUI text;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,16 +28,19 @@ public class MenuButton : MonoBehaviour
         if (collision.gameObject.layer == 7)
         {
             spriteRenderer.color = new Color(100, 0, 0);
+            text.text = "Rosso";
         }
 
         if (collision.gameObject.layer == 12)
         {
             spriteRenderer.color = new Color(0, 100, 0);
+            text.text = "Verde";
         }
 
         if (collision.gameObject.layer == 13)
         {
             spriteRenderer.color = new Color(0, 0, 100);
+            text.text = "Blu";
         }
         Debug.Log("Selezione");
         
