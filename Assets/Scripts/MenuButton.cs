f77babe99b0ba07ef1b0c425dd3357d7e79d7c8a
PCristian00 <cristianp9300@gmail.com>
2023-06-09 19:48:19 +0200

    Demo cambio colore con sparo

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MenuButton : MonoBehaviour
 {
+
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class MenuButton : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Selezione");
+        spriteRenderer.color = new Color(100,0,0);
         Destroy(collision.gameObject);
     }
 }
