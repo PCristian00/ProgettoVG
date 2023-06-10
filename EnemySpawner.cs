ec585c5c16b84acf91661d5a6eaf6406272a0ed2
MicheleA0 <134079380+MicheleA0@users.noreply.github.com>
2023-06-10 11:46:40 +0200

    Add files via upload

---
+++ EnemySpawner.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject [] enemies;
+    public GameObject Boss;
+    public float maxSpawnRate = 5f; // tempo di spawn
+    public float timer = 0;
+    public float countSpawn = 0;
+    public float countEnemyKill = 0;
+    public System.Action<EnemySpawner> killed;
+
+    // Ancora non implementata
+    public bool bossIsAlive;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countEnemyKill < 10f) 
+        {
+            if (countSpawn < 3f)
+            {
+                if (timer < maxSpawnRate)
+                {
+                    timer += Time.deltaTime;
+                }
+                else
+                {
+                    SpawnEnemy();
+                    timer = 0;
+                }
+            }
+        }
+        else if (!bossIsAlive)
+        {
+            SpawnBoss();
+        }
+        
+    }
+    void SpawnEnemy()
+    {
+        countSpawn++;
+        Debug.Log(countSpawn);
+        //prelevo i bordi della camera
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        max.x = 6f;
+        min.x = -6f;
+        max.y -= 3f;
+        //GameObject[] enemies = { Enemy_1, Enemy_2 };
+        GameObject enemy = Instantiate(enemies[Random.Range(0, enemies.Length)]);
+        enemy.transform.position = new Vector2(Random.Range(min.x, max.x), Random.Range(1, max.y));
+        ScheduleNextEnemySpawn();
+    }
+    void SpawnBoss()
+    {
+        bossIsAlive = true;
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        max.x = 6f;
+        min.x = -6f;
+        max.y -= 3f;
+        Instantiate(Boss);
+        Boss.transform.position = new Vector2(Random.Range(min.x, max.x), Random.Range(1, max.y));
+    }
+
+
+    void ScheduleNextEnemySpawn()
+    {
+        float spawnInNSeconds;
+        if (maxSpawnRate > 1f)
+        {
+            spawnInNSeconds = Random.Range(1f, maxSpawnRate);
+        }
+        else
+        {
+            spawnInNSeconds = 1f;
+        }
+    }
+
+    // FORSE INUTILIZZATA
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            killed?.Invoke(this);
+        }
+    }
+
+
+
+}
