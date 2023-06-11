0122f50a62ebbfe8d9bda380d0b88cf29c9c6ec8
PCristian00 <cristianp9300@gmail.com>
2023-06-11 12:25:42 +0200

    Spostamento laterale Boss
    Il boss si sposta da un limite dello schermo all'altro

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -6,6 +6,11 @@ public class BossScript : MonoBehaviour
     public int boss_life = 3;
     public LogicScript logic;
     public GameObject[] powerups;
+    
+    private Vector2 endPos;
+    public int direction;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -14,12 +19,49 @@ public class BossScript : MonoBehaviour
         spawner = GameObject.FindGameObjectWithTag("Respawn").GetComponent<EnemySpawner>();
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         logic.ShowMessage("ATTENZIONE!!!", 1);
+
+        endPos = new Vector2(transform.position.x, transform.position.y);
+
+        direction = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x == 6 || transform.position.x == -6)
+        {
+            direction *= -1;
+        }
+        Move(direction);
+    }
 
+    private void Move(float moveRate)
+    {
+        endPos.x += moveRate * Time.deltaTime;
+        endPos.x = Mathf.Clamp(endPos.x, -6, 6);
+        transform.position = endPos;
+
+        if (moveRate != 0)
+        {
+            if (Mathf.Sign(moveRate) == -1)
+            {
+                // Debug.Log("VERSO SINISTRA");
+                // spriteRenderer.sprite = sprites[1];
+                transform.rotation = Quaternion.Euler(0, 0, 10);
+            }
+            else if (Mathf.Sign(moveRate) == 1)
+            {
+                // Debug.Log("VERSO DESTRA");
+                // spriteRenderer.sprite = sprites[2];
+                transform.rotation = Quaternion.Euler(0, 0, -10);
+            }
+        }
+        else
+        {
+            // Debug.Log("Sto fermo");
+            // spriteRenderer.sprite = sprites[0];
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
