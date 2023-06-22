142ebd82a5cef5fa172e303b055d286bc5f78c38
PCristian00 <cristianp9300@gmail.com>
2023-06-22 15:50:32 +0200

    Rimossi riferimenti inutili in StarSpawner

--- Assets/Scripts/Stars/StarSpawner.cs
+++ Assets/Scripts/Stars/StarSpawner.cs
@@ -3,18 +3,11 @@ using UnityEngine;
 public class StarSpawner : MonoBehaviour
 {
     public GameObject star;
-    //public float spawnRate = 5;
-
-    public LogicScript logic;
+    public float spawnRate = 4;
 
     private float timer = 0;
 
     public int starCount = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-    }
 
     // Update is called once per frame
     void Update()
